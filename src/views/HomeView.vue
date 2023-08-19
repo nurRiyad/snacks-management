1386bcdb29415b13045d9d8c539f60517463ab66
nurRiyad <asadnurriyad@gmail.com>
2023-08-19 15:46:43 +0600

    Get user in home page
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import Hero from '@/components/Hero.vue'
+import { useSnacksStore } from '@/stores/counter'
+
+const snacksStore = useSnacksStore()
+
+snacksStore.getUser()
 </script>
 
 <template>
