123a01542499007cb0350d00b1e6ccf6fed606a3
zubairrafi <walleeva2018@gmail.com>
2024-02-20 01:21:34 +0600

    populated Home page info
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
-import Hero from '@/components/Hero.vue'
-import { useSnacksStore } from '@/stores/counter'
+import { type Order, useSnacksStore } from '@/stores/counter'
 
 const snacksStore = useSnacksStore()
 
 snacksStore.getUser()
+snacksStore.getSnacksEnableUser()
+snacksStore.getOrders()
+
+function calculate(item: Order[] | undefined) {
+  let items = ''
+  let cost = 0
+  item?.forEach((order: Order) => {
+    if (items !== '')
+      items += ','
+    items += order.name
+    cost += order.cost
+  })
+
+  items += `. Amount => ${cost}`
+  return items
+}
 </script>
 
 <template>
@@ -14,9 +29,69 @@ snacksStore.getUser()
         /snacks
       </RouterLink> for order
     </h1>
-    <Hero />
-    <h1 class="text-xl pt-5 font-bold ">
-      Start Your Day With Motivation !
-    </h1>
+
+    <div class="flex justify-between gap-12 text-center">
+      <h1 class="text-center text-xl text-primary font-bold font-sans  mr-80">
+        Floor:  <span class="text-bold"> 5 </span>
+      </h1>
+      <h1 class="text-center text-xl text-primary font-bold font-sans pb-5">
+        Floor:  <span class="text-bold"> 1 </span>
+      </h1>
+    </div>
+    <div class="flex justify-between gap-4">
+      <!-- First Table -->
+      <div class="overflow-x-auto border rounded-md flex-1">
+        <table class="table table-zebra">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Orders</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(snacks, idx) in snacksStore.snacksEnabledUsers" :key="snacks?.id + idx ">
+              <td v-if="snacks.floor === 5">
+                {{ idx }}
+              </td>
+              <td v-if="snacks.floor === 5">
+                {{ snacks.name }}
+              </td>
+              <td v-if="snacks.floor === 5">
+                {{ calculate(snacks.orders) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- Second Table -->
+      <div class="overflow-x-auto border rounded-md flex-1">
+        <table class="table table-zebra">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Orders</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(snacks, idx) in snacksStore.snacksEnabledUsers" :key="snacks?.id + idx ">
+              <td v-if="snacks.floor === 1">
+                {{ idx }}
+              </td>
+              <td v-if="snacks.floor === 1">
+                {{ snacks.name }}
+              </td>
+              <td v-if="snacks.floor === 1">
+                {{ calculate(snacks.orders) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
